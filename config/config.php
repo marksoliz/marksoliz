6f950ceae20609aaaef9843dfcd6d6dfe9977875
marksoliz <soliz.mark@gmail.com>
2025-05-01 16:58:53 -0500

    *

--- config/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-const DB_HOST = 'localhost'; // your database host
-const DB_USER = 'marksoliz_marksoliz'; // your database username
-const DB_PASS = ')W$-nz!EUZuE)+GlMr'; // your database password
-const DB_NAME = 'marksoliz_ms'; // your database name
