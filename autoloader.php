2272c5ccc56870003b9cae05a42626e1d0d83134
marksoliz <soliz.mark@gmail.com>
2025-05-01 20:40:46 -0500

    Changes to be committed: 	new file:   autoloader.php 	modified:   init.php

---
+++ autoloader.php
@@ -0,0 +1,21 @@
+<?php
+// filepath: c:\xampp\htdocs\marksoliz\app\autoloader.php
+
+spl_autoload_register(function ($class_name) {
+    $paths = [
+        __DIR__ . '/controllers/',
+        __DIR__ . '/models/',
+        __DIR__ . '/classes/',
+    ];
+
+    foreach ($paths as $path) {
+        $file = $path . $class_name . '.php';
+        if (file_exists($file)) {
+            require_once $file;
+            return;
+        }
+    }
+
+    // If the class is not found, throw an error
+    throw new Exception("Class $class_name not found.");
+});
